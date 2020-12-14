71742b86b9e716100784f6db0b500cf194c9a4ba
Rafael <rafamarinho87@gmail.com>
2020-12-14 16:35:20 -0300

    Endpoint /clientes/{id} disponivel

---
+++ src/main/java/com/rafael/cursomc/services/ClienteService.java
@@ -0,0 +1,23 @@
+package com.rafael.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.rafael.cursomc.domain.Cliente;
+import com.rafael.cursomc.repositories.ClienteRepository;
+import com.rafael.cursomc.services.exception.ObjectNotFoundException;
+
+@Service
+public class ClienteService {
+	
+	@Autowired
+	private ClienteRepository repo;
+
+	public Cliente buscar(Integer id) {
+		Optional<Cliente> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+		"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
+	}
+}
