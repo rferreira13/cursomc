0c83bf7ef53fae062b37a05416db7d51b8c9f00d
Rafael <rafamarinho87@gmail.com>
2021-01-08 21:55:24 -0300

    Endpoint /pedidos/{id} disponibilizado

---
+++ src/main/java/com/rafael/cursomc/services/PedidoService.java
@@ -0,0 +1,23 @@
+package com.rafael.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.rafael.cursomc.domain.Pedido;
+import com.rafael.cursomc.repositories.PedidoRepository;
+import com.rafael.cursomc.services.exception.ObjectNotFoundException;
+
+@Service
+public class PedidoService {
+	
+	@Autowired
+	private PedidoRepository repo;
+
+	public Pedido buscar(Integer id) {
+		Optional<Pedido> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+		"Objeto não encontrado! Id: " + id + ", Tipo: " + Pedido.class.getName()));
+	}
+}
